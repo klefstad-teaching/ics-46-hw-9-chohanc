197936eb8eb2d145037fc075f18ba47385c4382f
chohanc <chohanc@uci.edu>
2025-03-12 23:10:28 +0000

    fixed print

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -90,15 +90,11 @@ void load_words(set<string> & word_list, const string& file_name)
 
 void print_word_ladder(const vector<string>& ladder)
 {
-    if (ladder.empty())
-    {
-        error("", "", "Empty ladder.");
-    }
+    cout << "Words ladder found: "
 
     for (size_t i = 0; i < ladder.size(); ++i)
     {
-        cout << ladder[i];
-        if (i != ladder.size() - 1) cout << " -> ";
+        cout << ladder[i] << " ";
     }
 
     cout << endl;
